bd07fecba2ebce3193a6809b7b887da481386803
Thorben Bender <bender.thorben0@gmail.com>
2019-01-29 15:30:43 +0100

    add event listener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,47 @@
 // Your code goes here
+const btn = document.querySelector('.btn');
+btn.addEventListener('mouseover', function(){
+    btn.style.backgroundColor = 'green';
+})
+
+const logo = document.querySelector('.logo-heading');
+logo.addEventListener('keydown', (e) => {
+    let keycode = e.keycode;
+    console.log(keycode);
+    if (keycode === 'e'){
+        logo.style.color = 'green';
+    }
+}, false)
+
+
+const welcomeTo = document.querySelector('.intro h2');
+welcomeTo.addEventListener('wheel', () => {
+    welcomeTo.style.color = 'green';
+})
+
+document.body.addEventListener('load', () => {
+    document.body.style.color = 'green';
+})
+
+document.body.addEventListener('resize', () => {
+    document.body.style.color = 'yellow';
+})
+
+// .addEventListener('scroll', () => {
+
+// })
+document.querySelector('.intro').prepend('<input type="text" placeholder="Type in your name"/>')
+.addEventListener('select', () => {
+    
+})
+
+
+const letsGo = document.querySelector('.content-section h2');
+letsGo.addEventListener('dbclick', () => {
+    letsGo.style.color = 'green';
+})
+
+const navbar = document.querySelector('.nav a')
+navbar.addEventListener('click', function(event){
+    event.preventDefault();
+})
